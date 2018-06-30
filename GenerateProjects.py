f22f68e5850a8788a3737e6629d0c215b8e91c9b
Andy Carter <andy1704@gmail.com>
2018-06-30 17:49:03 +0100

    Rename settings

--- GenerateProjects.py
+++ GenerateProjects.py
@@ -6,7 +6,7 @@ settings = sublime.load_settings('GenerateProjects.sublime-settings')
 class GenerateProjectsCommand(sublime_plugin.WindowCommand):
 
     def run(self):
-        path = settings.get('project_files_directory')
+        path = settings.get('local_repositories_directory')
         for name in os.listdir(path):
             if os.path.isdir(os.path.join(path, name)) and not self.is_blacklisted(name):
                 self.generateProject(name)
@@ -18,8 +18,8 @@ class GenerateProjectsCommand(sublime_plugin.WindowCommand):
 
     def generateProject(self, name):
         """Generate a Sublime Text project file"""
-        project_path = settings.get('project_files_directory') + '/' + name
-        project_settings_path = settings.get('sublime_project_settings_directory')
+        project_path = settings.get('local_repositories_directory') + '/' + name
+        project_settings_path = settings.get('sublime_project_directory')
         project_file = project_settings_path + '/' + name + '.sublime-project'
         if not os.path.isfile(project_file):
             project = {'name': name, 'folders': [{'path': project_path}]}
